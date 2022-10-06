ce5a9b5ce4cab2ce00e365fed567cf988067869b
Sérgio Gabriel <75457521+sergab11@users.noreply.github.com>
2022-10-06 12:51:10 -0300

    Update return-page.js

--- T1/Javascript/return-page.js
+++ T1/Javascript/return-page.js
@@ -8,6 +8,8 @@
 // página return-page.html aparece quando ocorre o fim do jogo...
 // jogador tem a opção de recomeçar a partida (botaoJoga) ou de retornar ao menu principal (botaoRetorna)
 
+let serieEscolhida = window.location.href.substring(window.location.href.indexOf('#')+1);
+
 onload = function(){
     var botaoJoga = this.document.getElementById("btJoga");
     var botaoRetorna = this.document.getElementById("btRetorna"); 
@@ -17,9 +19,9 @@ onload = function(){
 }
 
 function trataBotaoJoga(){
-    window.open("mem-game.html","_self");
+    window.location.href = "mem-game.html#"+serieEscolhida;
 }
 
 function trataBotaoRetorna(){
-    window.open("index.html","_self");
+    window.location.href = "index.html";
 }
